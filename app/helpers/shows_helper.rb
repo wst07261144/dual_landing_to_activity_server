797f500fa264bb1930deae65aab110e3c3df85f4
wst07261144 <wst07261144@gmail.com>
2014-01-23 10:22:26 +0800

    部分代码转移到helper里面

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -1,2 +1,18 @@
 module ShowsHelper
+
+  def bid_end? win
+    return win.nil?||!(win[:activity_id] == @bidlist[:activity_id]&&win[:bid_name]== @bidlist[:name])
+  end
+
+  def bid_invalid? win1,win
+    return win1 == 'true' && win[:name]=='竞价无效'
+  end
+
+  def bid_valid? win1,win
+    return win1=='true'&&win[:name]!='竞价无效'
+  end
+
+  def bid_running? win,bidlist
+    return win.nil?||!(win[:activity_id] == bidlist[:activity_id]&&win[:bid_name]== bidlist[:name])
+  end
 end
