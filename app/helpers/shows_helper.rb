f049990ac6f8713cd1a4cc516b19c6144e70a76a
wst07261144 <wst07261144@gmail.com>
2014-01-23 11:50:18 +0800

    将部分代码移到helper里面

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -17,4 +17,19 @@ module ShowsHelper
        return msg4+msg5+msg6
      end
    end
+
+   def show_activity_situation
+     if @win.nil?
+        return "活动正在进行中"
+     end
+     if !@win.nil?&&@win[:name]=='竞价无效'
+        return '本次竞价无效'
+     end
+     if !@win.nil?&&@win[:name]!='竞价无效'
+        msg1='获胜者:'+@win.name
+        msg2='出价:'+@win.price+'元'
+        msg3='手机号:'+@win.phone
+       return  msg1+msg2+msg3
+     end
+   end
 end
