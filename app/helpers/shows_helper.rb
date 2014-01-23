0df1076dcd825bfea030e99cddbae8df88379a91
wst07261144 <wst07261144@gmail.com>
2014-01-23 11:20:55 +0800

    部分代码移到helper里面

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -1,18 +1,20 @@
 module ShowsHelper
 
-  def bid_end? win
-    return win.nil?||!(win[:activity_id] == @bidlist[:activity_id]&&win[:bid_name]== @bidlist[:name])
-  end
-
-  def bid_invalid? win1,win
-    return win1 == 'true' && win[:name]=='竞价无效'
-  end
-
-  def bid_valid? win1,win
-    return win1=='true'&&win[:name]!='竞价无效'
-  end
-
-  def bid_running? win,bidlist
-    return win.nil?||!(win[:activity_id] == bidlist[:activity_id]&&win[:bid_name]== bidlist[:name])
-  end
+   def show_bid_situation
+     if @win.nil?||!(@win[:activity_id] == @bidlist[:activity_id]&&@win[:bid_name]== @bidlist[:name])
+       msg1 = '参与人数(' + @bidding_detail.length.to_s
+       msg2 = '/'
+       msg3 =  @num.length.to_s+')'
+       return msg1+msg2+msg3
+     end
+     if @win1=='true'&&@win[:name]=='竞价无效'
+       return '本次竞价无效'
+     end
+     if  @win1=='true'&&@win[:name]!='竞价无效'
+       msg4="获胜者:"+@win[:name]
+       msg5='出价:'+@win[:price]+'元'
+       msg6='手机号:'+@win[:phone]
+       return msg4+msg5+msg6
+     end
+   end
 end
